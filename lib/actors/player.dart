56d61932948e385c50429187a90d8ab65584ba05
Doh Landrine <landrinedoh22@gmail.com>
2024-12-02 07:10:38 +0100

    Made my player to be able to move left and right. When moving right, he faces right and when moving left he is fliped to face left, and he response to animations, that is, when running he runs, and when idling he idles.

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,22 +1,39 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/gestures.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerStatus { idle, running }
 
+enum PlayerDirection { left, right, none }
+
 class Player extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure> {
   String character;
-  Player({position, required this.character}) :super(position: position);
+  Player({position, required this.character}) : super(position: position);
+
+  // animation variables
   late SpriteAnimation idleAnimation;
   late SpriteAnimation runningAnimation;
   double animationDuration = 0.05;
+
+  // movement variables
+  PlayerDirection playerDirection = PlayerDirection.none;
+  double movementSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimation();
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
   }
 
   void _loadAllAnimation() {
@@ -39,5 +56,32 @@ class Player extends SpriteAnimationGroupComponent
         textureSize: Vector2.all(32),
       ),
     );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        dirX = dirX - movementSpeed;
+        current = PlayerStatus.running;
+        break;
+      case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        dirX = dirX + movementSpeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerStatus.idle;
+        break;
+      default:
+    }
+    velocity = Vector2(dirX, 0.0);
+    position = position + velocity * dt;
   }
 }
