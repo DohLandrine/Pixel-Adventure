489d8610d5f636f5e4ff29d9a2e007583d96159f
Doh Landrine <landrinedoh22@gmail.com>
2024-12-02 07:58:48 +0100

    Added Keyboard control

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -2,14 +2,16 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/src/services/hardware_keyboard.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerStatus { idle, running }
 
-enum PlayerDirection { left, right, none }
+enum PlayerDirection { left, right, idle }
 
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   String character;
   Player({position, required this.character}) : super(position: position);
 
@@ -19,7 +21,7 @@ class Player extends SpriteAnimationGroupComponent
   double animationDuration = 0.05;
 
   // movement variables
-  PlayerDirection playerDirection = PlayerDirection.none;
+  PlayerDirection playerDirection = PlayerDirection.idle;
   double movementSpeed = 100;
   Vector2 velocity = Vector2.zero();
   bool isFacingRight = true;
@@ -34,6 +36,25 @@ class Player extends SpriteAnimationGroupComponent
   void update(double dt) {
     _updatePlayerMovement(dt);
     super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    if (isLeftKeyPressed && isRightKeyPressed) {
+      playerDirection = PlayerDirection.idle;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else {
+      playerDirection = PlayerDirection.idle;
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
 
   void _loadAllAnimation() {
@@ -75,8 +96,9 @@ class Player extends SpriteAnimationGroupComponent
           isFacingRight = true;
         }
         dirX = dirX + movementSpeed;
+        current = PlayerStatus.running;
         break;
-      case PlayerDirection.none:
+      case PlayerDirection.idle:
         current = PlayerStatus.idle;
         break;
       default:
